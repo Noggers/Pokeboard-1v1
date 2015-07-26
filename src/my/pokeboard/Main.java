e587f9029217ee27ec8114d85eb53401886a2afa
MichaelJ <wastekid0@gmail.com>
2015-07-26 15:40:51 -0500

    Added some exception handling, and brought in a using statement to begin handling resource downloading

--- src/my/pokeboard/Main.java
+++ src/my/pokeboard/Main.java
@@ -5,42 +5,64 @@
  */
 package my.pokeboard;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 import my.pokeboard.Views.PokeboardUI;
+import my.pokeboard.Views.ResourceDownloader;
 
 /**
  *
  * @author Michael
  */
 public class Main
-{    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[])
-    {
-        setApplicationStyle();
-        java.awt.EventQueue.invokeLater(() -> new PokeboardUI().setVisible(true));
-    }
-    
-    private static void setApplicationStyle()
-    {
-        // We search for the Nimbus look and feel
-        // If it's not available, we just use the default
-        try
-        {
-            javax.swing.UIManager.LookAndFeelInfo[] infos =  javax.swing.UIManager.getInstalledLookAndFeels();
-            for (javax.swing.UIManager.LookAndFeelInfo info : infos)
-            {
-                if (info.getName().equals("Nimbus"))
-                {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        }
-        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex)
-        {
-            java.util.logging.Logger.getLogger(PokeboardUI.class.getName()).log(java.util.logging.Level.INFO, null, ex);
-        }
-    }
+{
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String args[])
+	{
+		setApplicationStyle();
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					//TODO Add behavior for launching the resource downloader
+//					JFrame frame = new ResourceDownloader();
+					JFrame frame = new PokeboardUI();
+					frame.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private static void setApplicationStyle()
+	{
+		// We search for the Nimbus look and feel
+		// If it's not available, we just use the default
+		try
+		{
+			javax.swing.UIManager.LookAndFeelInfo[] infos = javax.swing.UIManager.getInstalledLookAndFeels();
+			for (javax.swing.UIManager.LookAndFeelInfo info : infos)
+			{
+				if (info.getName().equals("Nimbus"))
+				{
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		}
+		catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| javax.swing.UnsupportedLookAndFeelException ex)
+		{
+			java.util.logging.Logger.getLogger(PokeboardUI.class.getName()).log(java.util.logging.Level.INFO, null, ex);
+		}
+	}
 }
