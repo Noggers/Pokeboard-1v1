b375e53b417708fd09c0d2aec1d190868f28b431
Michael Jones <wastekid0@gmail.com>
2016-06-07 21:16:06 -0500

    Making the home directory for the user more platform-independent.

--- python/src/my/pokeboard/deck/__main__.py
+++ python/src/my/pokeboard/deck/__main__.py
@@ -1,3 +1,4 @@
+from os.path import expanduser
 from DeckConsoleInterface import run_console
 from PokemonCard import PokemonCard
 from DeckManager import DeckManager
@@ -8,6 +9,8 @@ Launches deck manager by asking the user for a file path to the saved deck build
 Then, we want to constitute that into a list of cards, and feed it to a DeckManager
 From there on out, we want the console to constantly sit and wait for messages from the user
 """
+
+home = expanduser("~")
 
 
 def read_card_file(file_path):
@@ -24,7 +27,7 @@ def read_card_file(file_path):
 print "Loading Deck Manager..."
 deck_path = raw_input("Please enter path to deck file (empty: default): ")
 if deck_path == "":
-    deck_path = "/Users/michaelj/Documents/Pokeboard/default.dk"
+    deck_path = home + "/Documents/Pokeboard/default.dk"
 print "Loading deck file from " + deck_path
 deck_build = read_card_file(deck_path)
 print "Deck loaded. Showing:"
